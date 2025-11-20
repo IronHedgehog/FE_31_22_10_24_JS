91e878af75cf65c5cca508f2a14d024a916b3cdc
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-11-20 18:44:00 +0200

    types

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// const, let
+
+// const name = "Artem";
+
+// let age = 18;
+// console.log(age);
+
+// age = 19;
+
+// console.log(age);
+
+// const age = prompt("Скільки тобі років?");
+// console.log(age);
+
+// console.log(18 - age);
+
+const name = "Artem"; // string
+const age = 18; // Number
+const isHappy = true; // boolean true ,false
+let empty = null; // null
+const undef = undefined; //undefined
+
+// symbol
+// bigint;
+
+// empty = 1;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isHappy);
+console.log(typeof empty);
+console.log(typeof undef);
+
+console.log("" + 18 - 10);
+console.log(18 - 10 + "" + 10);
