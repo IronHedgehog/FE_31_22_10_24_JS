79a6bfa4e9826ffa6587f900dc2a3c366589867b
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-12-04 20:36:48 +0200

    Strings

--- js/index.js
+++ js/index.js
@@ -19,3 +19,43 @@ const message = "sAle";
 if (spamWord.toLowerCase() == message.toLowerCase()) {
   console.log("ЦЕ спам");
 }
+
+const banWord = "BanWord is here";
+
+console.log(banWord.toLowerCase().includes("banword")); // true
+
+const email = "        asd@gmail.com              ";
+
+console.log(email.trim());
+
+const anyWord = "word";
+
+console.log(anyWord.slice(0, -1));
+
+// replace;
+
+const badWord = "badWord was here";
+
+console.log(badWord.toLowerCase().replace("badword", "goodWord"));
+
+// indexOf
+
+const word = "wordr";
+//indexOf - знайти індекс букви у слові
+const rIndex = word.toLowerCase().indexOf("r");
+
+console.log(rIndex);
+word.slice(rIndex, rIndex + 1);
+
+console.log(word);
+
+const userEmail = "asd@gmail.com";
+
+const dozvil = ".com";
+
+console.log(userEmail.endsWith(dozvil));
+// console.log(userEmail.startsWith(dozvil));
+
+const hello = "Hello!";
+
+console.log(hello.indexOf("H"));
