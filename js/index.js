e6f25d2e3065a77735abd757312d27ee1ff1f3c9
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-12-09 21:35:16 +0200

    ifelse

--- js/index.js
+++ js/index.js
@@ -1,61 +1,40 @@
-const greetings = "Hello";
+console.log(true && true && "ВАУ ЦЕ ПРАВДА"); // false
+console.log("object" || (1 && 0)); //
 
-const myString = "Artem";
-const string = "a";
-const string1 = `a`;
-console.log(myString.length);
+// "object"; -- true
+// ||
+//  1 && 0; -- false
 
-console.log(greetings + " " + myString); // конкатинація = додавання рядків
-console.log(`${greetings} ${myString}`); // інтерполяція
+// console.log("" || "kivi" || ("mango" && !0));
 
-const test = "test";
+// console.log("" || "kivi");
+// console.log("kivi" || );
+// console.log("mango" && !0);
 
-console.log(test.toUpperCase());
-console.log(test.toLowerCase());
+let goWalk = false;
 
-const spamWord = "sale";
-const message = "sAle";
+// const dish = confirm("Чи ти помив посуд");
 
-if (spamWord.toLowerCase() == message.toLowerCase()) {
-  console.log("ЦЕ спам");
+// const doHomework = confirm("Чи ти виконав домашнє завдання");
+
+// if (dish && doHomework) {
+//   goWalk = true;
+// }
+// Якщо інакше
+if (goWalk === true) {
+  console.log("Гулять");
+} else {
+  console.log("Гуляти не ідем");
 }
 
-const banWord = "BanWord is here";
+const cost = 500;
 
-console.log(banWord.toLowerCase().includes("banword")); // true
-
-const email = "        asd@gmail.com              ";
-
-console.log(email.trim());
-
-const anyWord = "word";
-
-console.log(anyWord.slice(0, -1));
-
-// replace;
-
-const badWord = "badWord was here";
-
-console.log(badWord.toLowerCase().replace("badword", "goodWord"));
-
-// indexOf
-
-const word = "wordr";
-//indexOf - знайти індекс букви у слові
-const rIndex = word.toLowerCase().indexOf("r");
-
-console.log(rIndex);
-word.slice(rIndex, rIndex + 1);
-
-console.log(word);
-
-const userEmail = "asd@gmail.com";
-
-const dozvil = ".com";
-
-console.log(userEmail.endsWith(dozvil));
-// console.log(userEmail.startsWith(dozvil));
-
-const hello = "Hello!";
-
-console.log(hello.indexOf("H"));
+if (cost <= 299) {
+  console.log("Підписка стандартна");
+} else if (cost >= 1000) {
+  console.log("Максимальна");
+} else if (cost >= 300) {
+  console.log("Medium");
+} else {
+  console.log(" У нас немає такого дорогого пакету бери інший");
+}
