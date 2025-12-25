1ab7b7b8b3cc752e92f61a8b0b7dba537b5d6baf
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-12-25 21:20:38 +0200

    loops

--- js/index.js
+++ js/index.js
@@ -31,18 +31,73 @@
 //   console.log(word[i]);
 // }
 
-let processes = true;
+// let processes = true;
 
-while (processes) {
-  let userChoice = prompt("Введіть назву напою(Кава, чай, сік)");
+// while (processes) {
+//   let userChoice = prompt("Введіть назву напою(Кава, чай, сік)");
 
-  if (userChoice == "off") {
-    processes = false;
-  } else {
-    console.log(" Ось ваше замовлення");
-    processes = false;
-    // break;
+//   if (userChoice == "off") {
+//     processes = false;
+//   } else {
+//     console.log(" Ось ваше замовлення");
+//     processes = false;
+//     // break;
+//   }
+
+//   // processes = false;
+// }
+
+let energy = 100;
+let health = 100;
+
+let strength = 7;
+let agility = 5;
+let intelligence = 3;
+
+// while -нескінченний
+// перестає виконуватись тільки тоді коли умова стає не правдою
+
+// while (energy > 0) {
+//   let choice = prompt("Яку дію ви бажаєте виконати? fight/training/chill ");
+
+//   if (choice === "chill") {
+//     energy += 10;
+//   } else if (choice === "training") {
+//     energy -= 10;
+//     intelligence += 1;
+//     agility += 1;
+//     strength += 1;
+//     alert(
+//       `тренування пройшло успішно, твої скіли підвищено strength = ${strength},agility = ${agility}, intelligence = ${intelligence}, energy = ${energy}`
+//     );
+//   } else if (choice === "fight") {
+//     if (energy < 30 && health < 30) {
+//       alert("ресурсів недостатньо, піди відпочинь");
+//     }
+//     health -= 30;
+//     energy -= 30;
+//     alert(
+//       `Ми відвідали турнір і там щось було. Тепер енергії ${energy} а здоровʼя стіки${health}`
+//     );
+//   } else {
+//     alert("ця функція поки не робе");
+//   }
+// }
+
+// for - має початок і має кінець
+
+// let i = 5;
+
+// while (i >= 0) {
+//   console.log(i);
+//   i--;
+// }
+
+//  ітерація - одне виконання
+for (let counter = 0; counter <= 10; counter++) {
+  if (counter % 2 === 0) {
+    console.log(counter);
+    continue;
   }
-
-  // processes = false;
+  console.log(counter);
 }
