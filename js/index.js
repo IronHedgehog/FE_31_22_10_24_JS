c4af77b2592aebb0624bf6fb69efebf8404111d5
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-01-15 20:50:14 +0200

    functions

--- js/index.js
+++ js/index.js
@@ -3,14 +3,23 @@
 // () - ?
 // {} - тіло функції (сюди пишемо усе що функція має зробити)
 function add(number1 = 0, number2 = 0) {
-  console.log(number1, number2);
-  console.log(number1 + number2);
+  if (number1 === 0 && number2 === 0) return 0;
+  let result = 0;
+  // Повертає результат вашої функції
+  // return - повертає результат функції та зупиняє функцію
+
+  result = number1 + number2;
+
+  // console.log(result);
+  return result;
 }
 
-console.log(5);
-console.log(6);
-console.log(7);
 // Щоб функція відпрацювала її треба викликати
 // 1. звернутись по імені
 // 2 поставити круглі дужки
-add(10, 15);
+add(10, 15); // виклик дії
+const s2 = add(15, 25);
+console.log(s2);
+const result = add(20, 30);
+
+console.log(result);
