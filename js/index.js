454db02aaa2fbf108c8fd0072ac5afb499432218
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-06-25 18:49:54 +0300

    Observer

--- js/index.js
+++ js/index.js
@@ -1,27 +1,20 @@
-const scrollNormal = document.querySelector(".vanilla");
-const scrollThrottled = document.querySelector(".throttled");
-const scrollDebounce = document.querySelector(".trailing");
+const list = document.querySelector(".list");
+let lastElement = list.lastElementChild;
+console.log(lastElement);
 
-let counterVanila = 0;
-let counterThtrotled = 0;
-let scrollDebounced = 0;
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((entity) => {
+    if (entity[0].isIntersecting) {
+      const img = document.createElement("img");
+      img.src = "https://placehold.co/600x800";
+      list.appendChild(img);
+      observer.unobserve(lastElement);
+      lastElement = list.lastElementChild;
+      observer.observe(lastElement);
+    }
+  });
 
-const scrollHandlerVanila = (e) => {
-  scrollNormal.textContent = counterVanila;
-  counterVanila++;
-};
-const scrollHandlerThrotled = (e) => {
-  scrollThrottled.textContent = counterThtrotled;
-  counterThtrotled++;
-};
-
-const scrollHandlerDebounce = (e) => {
-  scrollDebounce.textContent = scrollDebounced;
-  scrollDebounced++;
-};
-
-document.addEventListener("scroll", scrollHandlerVanila);
-
-document.addEventListener("scroll", _.throttle(scrollHandlerThrotled, 300));
-
-document.addEventListener("scroll", _.debounce(scrollHandlerDebounce, 300));
+  observer.observe(lastElement);
+} else {
+  console.log("Не підтримується");
+}
