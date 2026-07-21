a9a187807f2ae122ae8412cff34c2957f034015b
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-07-21 19:58:53 +0300

    handlebars

--- index.js
+++ index.js
@@ -1,26 +1,102 @@
-import Game from "./game.js";
-import { createPlayer } from "./player.js";
+import user from "./templates/user.hbs";
 
-const player = createPlayer();
-const game = new Game("Мапа", "gold", "goblin");
-console.log(game.createGame());
+// що таке шаблонізація; ПРоблематика(нашо воно треба?)
+// використовувати Handlebars; підключення та використання
+// створювати шаблони;
+// Поєднати з parcel
+// працювати з окремими .hbs файлами;
+// використовувати {{}}, #each, #if;
+// рендерити великі списки;
 
-function attack() {
-  console.log(`${playerName} attacks`);
-}
+const users = [
+  {
+    name: "Alex",
+    age: 20,
+  },
+  {
+    name: "Kate",
+    age: 15,
+  },
+  {
+    name: "John",
+    age: 25,
+  },
+];
 
-function heal() {
-  console.log(`${playerName} heals`);
-}
+// createElement;
 
-function buySword() {
-  console.log(`${playerName} bought sword`);
-}
+// insertAdjesentHtml;
+// innerHtml;
+// append;
+// appendChild;
+// 2 Знайти шаблон
+// const shablon = document.getElementById("template");
 
-function buyPotion() {
-  console.log(`${playerName} bought potion`);
-}
+//3 Підготувати шаблон до роботи(Скомпілювати та переробити у рядок)
+// const tempalteMarkup = Handlebars.compile(shablon.innerHTML);
+// console.log(tempalteMarkup);
+// 4 Передати дані у скомпільований шаблон.
+// const html = tempalteMarkup({
+//   name: "Alex",
+//   age: 20,
+// });
+// console.log(html);
+// 5 готову розмітку закинуть на екран куди потрібно
+// document.body.innerHTML = html;
 
-function saveGame() {
-  console.log("Saved");
-}
+// const data = {
+//   cars: ["BMW", "AUDI", "Mers"],
+// };
+
+// const shablonCars = document.querySelector(".cars").innerHTML;
+// console.log(shablonCars);
+
+// const tempalteMarkupCars = Handlebars.compile(shablonCars);
+
+// const htmlCars = tempalteMarkupCars(data);
+
+// console.log(htmlCars);
+// document.body.innerHTML = htmlCars;
+
+// export const games = [
+//   {
+//     title: "Minecraft",
+//     price: 1200,
+//     genre: "Sandbox",
+//     available: true,
+//     img: "...",
+//   },
+
+//   {
+//     title: "CS2",
+//     price: 0,
+//     genre: "Shooter",
+//     available: false,
+//     img: "...",
+//   },
+
+//   {
+//     title: "Terraria",
+//     price: 300,
+//     genre: "Adventure",
+//     available: true,
+//     img: "...",
+//   },
+// ];
+
+// npm init -y
+// npm install --save-dev parcel
+// npm install handlebars
+// npm install --save-dev parcel-transformer-hbs
+// створення .parcelrc з наступним кодом
+// {
+//   "extends": "@parcel/config-default",
+//   "transformers": {
+//     "*.hbs": ["parcel-transformer-hbs"]
+//   }
+// }
+
+const usersList = document.getElementById("users");
+
+console.log(user({ name: "Artem", age: 20 }));
+usersList.innerHTML = user(user1);
--- player.js
+++
@@ -1,9 +0,0 @@
-// name export
-
-export function createPlayer() {
-  return {
-    name: "Player",
-    level: 1,
-    gold: 100,
-  };
-}
